0a2828a0b30eddfa3b140a8751f172fe87b1b6a6
Anna Frolova <afrolova@tcinet.ru>
2026-02-21 14:03:50 +0300

    feat: unpack string

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,61 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var prevChar rune
+	var builder strings.Builder
+	var escapedPrevChar bool
+
+	for i, char := range str {
+		// Валидация: первый символ не может быть цифрой
+		if i == 0 && unicode.IsDigit(char) {
+			return "", ErrInvalidString
+		}
+
+		// Валидация: две цифры подряд
+		if unicode.IsDigit(char) && unicode.IsDigit(prevChar) && !escapedPrevChar {
+			return "", ErrInvalidString
+		}
+
+		// Валидация: экранируем только цифру или \
+		if prevChar == '\\' && !(unicode.IsDigit(char) || char == '\\') {
+			return "", ErrInvalidString
+		}
+
+		// Игнорируем \ и обрабатываем дальше
+		if prevChar == '\\' && !escapedPrevChar {
+			prevChar, escapedPrevChar = char, true
+			continue
+		}
+
+		// Обработка цифры (повторение предыдущего символа)
+		if unicode.IsDigit(char) {
+			count, err := strconv.Atoi(string(char))
+			if err != nil {
+				return "", fmt.Errorf("failed to parse count: %w", err)
+			}
+
+			// Повторяем предыдущий символ count раз
+			builder.WriteString(strings.Repeat(string(prevChar), count))
+		} else if prevChar != 0 && !unicode.IsDigit(prevChar) || escapedPrevChar { // Обработка обычного символа
+			builder.WriteRune(prevChar)
+		}
+
+		prevChar, escapedPrevChar = char, false
+	}
+
+	// Добавляем последний символ, если он не цифра
+	if prevChar != 0 && !unicode.IsDigit(prevChar) || escapedPrevChar {
+		builder.WriteRune(prevChar)
+	}
+
+	return builder.String(), nil
 }
