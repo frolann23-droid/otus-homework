151301971b7c9d7f160da2e52e16b38b2328752b
Anna Frolova <afrolova@tcinet.ru>
2026-04-13 14:13:14 +0300

    feat: pipeline execution

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,32 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	firstIn := make(Bi)
+
+	go func() {
+		defer close(firstIn)
+		for {
+			select {
+			case <-done:
+				return
+			case val, ok := <-in:
+				if !ok {
+					return
+				}
+
+				select {
+				case firstIn <- val:
+				case <-done:
+					return
+				}
+			}
+		}
+	}()
+
+	currentIn := In(firstIn)
+	for i := 0; i < len(stages); i++ {
+		currentIn = stages[i](currentIn)
+	}
+
+	return currentIn
 }
